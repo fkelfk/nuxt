1812b8b1fca3ba1fac37463a2862e61db590b6cf
fkelfk <fkelfk09@gmail.com>
2022-11-21 16:42:09 +0900

    feat: Add kakao oauth

---
+++ kakao-login-front/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <a @click="loginWithKakao">
+    <img src="/kakao_login_medium_narrow.png"/>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  methods: {
+    kakaoInit () {
+      Kakao.init('facc6cf0ffb6bf8618a80d5bbb561896')// KaKao client key
+      Kakao.isInitialized()
+    },
+    async loginWithKakao () {
+      await Kakao.Auth.authorize({
+        redirectUri: `${window.location.origin}/kakao-callback`
+      })
+    }
+  },
+  mounted () {
+    this.kakaoInit()
+  }
+}
+</script>
+
---
+++ kakao-login-front/pages/kakao-callback.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    로그인 중 입니다...
+  </div>
+</template>
+
+<script>
+export default {
+  async mounted () {
+    try {
+      // code 유무 체크
+      if (!this.$route.query.code) {
+        return this.$router.push('/')
+      }
+      console.log(`code : ${this.$route.query.code}`)
+
+      // 카카오 로그인 요청
+      const body = {
+        code: this.$route.query.code,
+        domain: window.location.origin
+      }
+      const response = await this.$axios.$post(
+        '/oauth/kakao', body, {}
+      )
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+</script>
